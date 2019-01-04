e5a319b1b41ccfc8ef8e1f4c1f7b7fc2dcab16f0
AzumiMaekawa <applle1111@gmail.com>
2019-01-04 23:59:57 +0900

    initial commit

---
+++ hello.py
@@ -0,0 +1,17 @@
+# pybullet first test file
+import pybullet as p
+import time
+import pybullet_data
+pysicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+cubeStartPos = [0,0,1]
+cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
+boxId = p.loadURDF("r2d2.urdf",cubeStartPos, cubeStartOrientation)
+for i in range(10000):
+    p.stepSimulation()
+    time.sleep(1./240.)
+cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
+print(cubePos,cubeOrn)
+p.disconnect()
