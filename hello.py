25e01ef3471bd206b295b2dbacd155a0d165d100
AzumiMaekawa <applle1111@gmail.com>
2019-01-06 00:51:48 +0900

    update

--- hello.py
+++ hello.py
@@ -1,14 +1,30 @@
 # pybullet first test file
+
 import pybullet as p
 import time
 import pybullet_data
+
 pysicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
 p.setGravity(0,0,-9.8)
+
+# camera view setting
+# args: cameraEyePosition, cameraTargetPosition, cameraUpVector
+#viewMatrix = p.computeViewMatrix([0.5,-0.5,0.5],[0,0,0],[-1,1,2])
+#p.getCameraImage(600,600,viewMatrix)
+
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+
+distance = 2
+yaw = 20
+height = -30
+p.resetDebugVisualizerCamera(distance, yaw, height, [0,0,0])
+
 planeId = p.loadURDF("plane.urdf")
 cubeStartPos = [0,0,1]
 cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
-boxId = p.loadURDF("r2d2.urdf",cubeStartPos, cubeStartOrientation)
+boxId = p.loadURDF("/urdf/lip_model01.urdf",cubeStartPos, cubeStartOrientation)
 for i in range(10000):
     p.stepSimulation()
     time.sleep(1./240.)
