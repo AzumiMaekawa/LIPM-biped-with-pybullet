53b44f3109716bca1737b1602665e8951ce5805b
AzumiMaekawa <applle1111@gmail.com>
2019-01-06 03:35:56 +0900

    test for realtime render video

--- hello.py
+++ hello.py
@@ -4,6 +4,7 @@ import pybullet as p
 import time
 import pybullet_data
 import cv2 as cv
+import numpy as np
 
 pysicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -28,11 +29,20 @@ cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
 boxId = p.loadURDF("/urdf/lip_model01.urdf",cubeStartPos, cubeStartOrientation)
 #p.startStateLogging(p.STATE_LOGGING_VIDEO_MP4,'test.mp4')
 
-image_path = "images/"
+image_path = "images/" # define a path for image restore
 p.setRealTimeSimulation(False)
+
+cameraParams = p.getDebugVisualizerCamera() # to get the width and height of rendering frame
+#print('width: {0}, height:{1}'.format(cameraParams[0],cameraParams[1]))
+image_width  = cameraParams[0]
+image_height = cameraParams[1]
+
 for i in range(10000):
     p.stepSimulation()
-    img=p.getCameraImage(500, 500, renderer=p.ER_BULLET_HARDWARE_OPENGL)
+    img_arr=p.getCameraImage(image_width,image_height, renderer=p.ER_BULLET_HARDWARE_OPENGL)
+    img = np.reshape(img_arr[2],(image_height,image_width,4)) 
+    img = cv.cvtColor(img, cv.COLOR_RGBA2BGRA)# convert RGBA image to BGRA image (openCV:BGRA)
+#    img = img*(1./255.)
     cv.imwrite(image_path+"{}.png".format(i), img)
     time.sleep(1./240.)
     if i == 500:
