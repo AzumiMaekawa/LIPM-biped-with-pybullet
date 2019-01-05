7bd0c88ca277ea17fe37c5560d8b3affb1828843
AzumiMaekawa <applle1111@gmail.com>
2019-01-06 02:52:58 +0900

    update

--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@
 import pybullet as p
 import time
 import pybullet_data
+import cv2 as cv
 
 pysicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -16,18 +17,29 @@ p.setGravity(0,0,-9.8)
 
 p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
-distance = 2
-yaw = 20
-height = -30
-p.resetDebugVisualizerCamera(distance, yaw, height, [0,0,0])
+distance = 1
+yaw = 45 
+pitch = -45
+p.resetDebugVisualizerCamera(distance,yaw,pitch, [0,0,0])
 
 planeId = p.loadURDF("plane.urdf")
-cubeStartPos = [0,0,1]
+cubeStartPos = [0,0,0.5]
 cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
 boxId = p.loadURDF("/urdf/lip_model01.urdf",cubeStartPos, cubeStartOrientation)
+#p.startStateLogging(p.STATE_LOGGING_VIDEO_MP4,'test.mp4')
+
+image_path = "images/"
+p.setRealTimeSimulation(False)
 for i in range(10000):
     p.stepSimulation()
+    img=p.getCameraImage(500, 500, renderer=p.ER_BULLET_HARDWARE_OPENGL)
+    cv.imwrite(image_path+"{}.png".format(i), img)
     time.sleep(1./240.)
+    if i == 500:
+        break
+    
+
+#p.stopStateLogging(STATE_LOGGING_VIDEO_MP4, test)
 cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
 print(cubePos,cubeOrn)
 p.disconnect()
